5628222ba7b2e0885df8c76610a6efc132810321
Laboratório FIAP <marcos.selmini@uol.com.br>
2026-03-23 20:53:38 -0300

    estrutura da classe Funcionario

---
+++ src/br/techcorp/funcionario/Funcionario.java
@@ -0,0 +1,59 @@
+package br.techcorp.funcionario;
+
+public class Funcionario {
+    private String matricula;
+    private String nome;
+    private String cargo;
+    private double salarioBase;
+    private double percentualBonus;
+    private boolean ativo;
+
+    public Funcionario(String matricula, String nome, String cargo, double salarioBase) {
+        this.matricula = matricula;
+        this.nome = nome;
+        this.cargo = cargo;
+        this.salarioBase = salarioBase;
+        this.ativo = true;
+    }
+
+    public double calcularSalarioLiquido() {
+
+    }
+
+    public boolean aplicarAumento(double percentual) {
+
+    }
+
+    public void aplicarBonus(double percentual) {
+
+    }
+
+    public void desligar() {
+        this.ativo = false;
+        this.percentualBonus = 0;
+    }
+
+    public boolean isAtivo() {
+        return ativo;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+}
